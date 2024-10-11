9b8fc7ffad84f9e8a09aa297d92e653567b403e4
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2024-10-11 10:03:58 -0400

    Make sure scripts/python3.sh does not result in datalad env

--- scripts/python3.sh
+++ scripts/python3.sh
@@ -2,5 +2,10 @@
 
 source scripts/utils.sh echo -n
 
+datalad --version >/dev/null && _DATALAD_PYTHON3=$(which python3)
+
 _python3_version=$(git config --file scripts/config/python3_config --get python3.version || echo -n)
-init_venv --name cp${_python3_version}/py --prefix .tmp/ -- python3 "$@"
+init_venv --name cp${_python3_version}/py --prefix .tmp/
+
+[[ -z "${_DATALAD_PYTHON3}" ]] || [[ "${_DATALAD_PYTHON3}" != "$(which python3)" ]]
+python3 "$@"
