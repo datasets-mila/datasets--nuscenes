33fc0e5297f0d0867782898ad2f365b202098e31
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2025-01-09 11:32:54 -0500

    Fix .dataset_utils
    not checkout when other submodules are present

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -3,7 +3,7 @@
 _SCRIPT_DIR=$(cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")" && pwd -P)
 
 _DATASETS_UTILS=$(git -C "${_SCRIPT_DIR}" rev-parse --show-toplevel)/.datasets_utils
-git -C "${_DATASETS_UTILS}/.." submodule status | grep -Ev "^[-+].*\.datasets_utils.*" || \
+git -C "${_DATASETS_UTILS}/.." submodule status | grep -Ev "^[-+].*\.datasets_utils.*" | grep ".*\.datasets_utils.*" || \
 	git -C "${_DATASETS_UTILS}/.." submodule update --init .datasets_utils
 
 source "${_DATASETS_UTILS}/utils.sh" echo -n
